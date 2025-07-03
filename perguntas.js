64707ade6d04e5dcd4dc7641c57b72d3fa47b9d3
tetheus4546b <vinicius.souza.matheus@escola.pr.gov.br>
2025-07-03 20:56:06 -0300

    Finalizando o trabalho

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,71 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Português',
+    'O que é parafrasear?',
+    'expressar a mesma ideia de um texto ou frase utilizando outras palavras, sem alterar o significado original'
+)
+
+criaCartao(
+    'Português',
+    'Qual é o verbo da palavra ponderar no pretérito mais que perfeito?',
+    'nós ponderáramos.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O Brasil é maior que os Estados Unidos?',
+    'Não, O Brasil com cerca de 8.515.767 km² e os Estados Unidos aproximadamente 9.833.517 km².'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual á menor fronteira do mundo em terra?',
+    'entre Marrocos e Espanha, na região de Peñón de Vélez de la Gomera, com apenas 85 metros de comprimento.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'História',
+    'Hitler nasceu aonde?',
+    'Hitler nasceu na Austria menos tendo trabalhado como um soldado alemão.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'História',
+    'quando começou a revolução industrial?',
+    'Na segunda metade do século XVIII, mais precisamente a partir de 1760.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quimica',
+    'Qual é o nome do elemento químico n°110 ?',
+    'O nome do elemento é Darmstádtio.'
+)
+
+criaCartao(
+    'Quimica',
+    'Calcule, ápos 18 horas, quantos mols ainda não terão decaído, considerando a variação temporal da constante de decaimento λ(t)? ( Use a formula N(t) = N₀ * e^(-λt))',
+    'Calculando a integral da taxa variável, obtemos um valor total de aproximadamente 1.042. Logo: N(18) = 13 * e^(-1.042)= 13 x 0,352 = 4,576 mols restantes.'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz em m/s ?',
+    'Sua velocidade é de 299.793.458 m/s.'
+)
+
+criaCartao(
+    'Física'
+    'Qual a força de pressão exercida pela luz solar numa estrutura reta, plana de exatos 2 m^2? Desconsidere atrito com ar.'
+    'F=6,67 x 10^-6 N ou 0,000666 N ou 0,0045g.'
+)
+
+criaCartao(
+    'Biologia'
+    'como uma celular utiliza a proteina?'
+    'As células utilizam proteínas de diversas formas, incluindo catalisar reações químicas, transportar moléculas, fornecer suporte estrutural e sinalizar outras células. As proteínas são essenciais para a função celular e estão envolvidas em quase todos os processos biológicos. '
+)
+
+criaCartao(
+    'Biologia'
+    'como pode ocorrer uma mutação genética?'
+    'As mutações genéticas ocorrem quando há alterações na sequência do DNA de um organismo. Elas podem acontecer espontaneamente, devido a erros na replicação do DNA ou serem induzidas por fatores externos, como exposição a radiação ou substâncias químicas. '
 )
